3ed8373aac89ffe971248a2a6c52faca42c06b8b
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-02-24 19:09:54 -0300

    Mudando questões para Português

--- src/data/Quizdata.js
+++ src/data/Quizdata.js
@@ -1,40 +1,88 @@
 const quiz = {
-    topic: 'Javascript',
-    level: 'Beginner',
-    totalQuestions: 4,
+    topic: 'Ortografia Acentuação',
+    level: 'Iniciante',
+    totalQuestions: 10,
     perQuestionScore: 1,
     questions: [
       {
         question:
-          'Which function is used to serialize an object into a JSON string in Javascript?',
-        choices: ['stringify()', 'parse()', 'convert()', 'None of the above'],
-        type: 'MCQs',
-        correctAnswer: 'stringify()',
+          'A ausencia de acento agúdo em "ideias" está em conformidade com as regras ortográficas vigentes',
+        choices: ['Verdadeiro', 'Falso'],
+        type: 'Objetiva',
+        correctAnswer: 'Verdadeiro',
+        explanaition: '"ideias" é paroxítona terminada em A(s)',
       },
       {
         question:
-          'Which of the following keywords is used to define a variable in Javascript?',
-        choices: ['var', 'let', 'var and let', 'None of the above'],
-        type: 'MCQs',
-        correctAnswer: 'var and let',
+          'A palavra "Está" recebe acento gráfico em decorrencia da mesma regra que determina o emprego do acento no vocábulo "três"',
+        choices: ['Verdadeiro', 'Falso'],
+        type: 'Objetiva',
+        correctAnswer: 'Falso',
+        explanaition: '"Está" é oxítona terminada em A, enquanto "Três" é monossílaba terminada em E(s)',
       },
       {
         question:
-          'Which of the following methods can be used to display data in some form using Javascript?',
-        choices: [
-          'document.write()',
-          'console.log()',
-          'window.alert',
-          'All of the above',
-        ],
-        type: 'MCQs',
-        correctAnswer: 'All of the above',
+          'Os vocábulos "Caráter", "Intrasferível" e "Órgãos" são acentuados em decorrência da regra gramatical que classifica as palavras paroxítonas',
+        choices: ['Verdadeiro', 'Falso'],
+        type: 'Objetiva',
+        correctAnswer: 'Verdadeiro',
+        explanaition: '',
       },
       {
-        question: 'How can a datatype be declared to be a constant type?',
-        choices: ['const', 'var', 'let', 'constant'],
-        type: 'MCQs',
-        correctAnswer: 'const',
+        question:
+          'As palavras "Líquida", "Público", "Órgãos" e "Episódicas" obedecem a mesma regra de acentuação gráfica',
+        choices: ['Verdadeiro', 'Falso'],
+        type: 'Objetiva',
+        correctAnswer: 'Falso',
+        explanaition: '"Órgãos" não é proparoxítona',
+      },
+      {
+        question:
+          'As palavras "Indivíduos" e "Precárias" recebem acento gráfico com base em justificativas gramaticais diferentes.',
+        choices: ['Verdadeiro', 'Falso'],
+        type: 'Objetiva',
+        correctAnswer: 'Falso',
+        explanaition: 'Ambas são paroxítonas terminadas em ditongo',
+      },
+      {
+        question:
+          'A palavra "Cível" recebe acento gráfico em decorrência da mesma regra que determina o emprego em "Amável" e "Útil"',
+        choices: ['Verdadeiro', 'Falso'],
+        type: 'Objetiva',
+        correctAnswer: 'Verdadeiro',
+        explanaition: 'Todas são paroxítonas não terminadas em A,E,O ou EM',
+      },
+      {
+        question:
+          'O emprego do acento gráfico em "Água", "Distância" e "Primário" justifica-se pela mesma regra de acentuação',
+        choices: ['Verdadeiro', 'Falso'],
+        type: 'Objetiva',
+        correctAnswer: 'Verdadeiro',
+        explanaition: '"São paroxítonas terminadas em ditongo',
+      },
+      {
+        question:
+          'O emprego do acento gráfico em "Indústria" e "Rádio" justifica-se com base na mesma regra',
+        choices: ['Verdadeiro', 'Falso'],
+        type: 'Objetiva',
+        correctAnswer: 'Verdadeiro',
+        explanaition: '"São paroxítonas terminadas em ditongo',
+      },
+      {
+        question:
+          'O emprego do acento gráfico nas palavras "metálica", "acúmulo" e "imóveis" se justifica com a mesma regra de acentuação',
+        choices: ['Verdadeiro', 'Falso'],
+        type: 'Objetiva',
+        correctAnswer: 'Falso',
+        explanaition: '"Imóveis" não é paroxítona',
+      },
+      {
+        question:
+          'Justifica-se com base na mesma regra de acentuação gráfica o emprego do acento gráfico nos vocábulos "Sabíamos" e "Processávamos"',
+        choices: ['Verdadeiro', 'Falso'],
+        type: 'Objetiva',
+        correctAnswer: 'Verdadeiro',
+        explanaition: 'Ambas são proparoxítonas',
       },
     ],
   }
